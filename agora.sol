d9fa448b476d43051dd7cd02969acb9442ff4319
"Aloysius" <"aloycwl@gmail.com">
2022-05-06 20:52:20 +0800

    initial

---
+++ agora.sol
@@ -0,0 +1,9 @@
+pragma solidity>0.8.0;//SPDX-License-Identifier:None
+//interface erc721 for nft
+//interface erc20 + eth contract address for purchase
+
+contract agora{
+    //list nfts function
+    //input function
+    //create allowance function
+}
